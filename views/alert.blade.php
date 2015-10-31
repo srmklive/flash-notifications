87b4c904056a1cab7bc55afe772a481de7982fca
Raza Mehdi <srmk@outlook.com>
2015-10-31 12:05:20 +0500

    Update alert.blade.php

--- views/alert.blade.php
+++ views/alert.blade.php
@@ -2,11 +2,11 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" />
 @endsection
 
-@if(session()->has('flashalert'))
-
 @section('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+
+@if(session()->has('flashalert'))
 <script>
 swal({
     title: "{{session('flashalert.title')}}",
@@ -16,6 +16,6 @@ swal({
     showConfirmButton: "{{config('flashalert.show_confirmation_button')}}"
 });
 </script>
-@endsection
-
 @endif
+
+@endsection
